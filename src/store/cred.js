e71182f13ae49325d36802f929a74fd4e8a575d2
Tomas Mali <tomas@iMac-di-Tomas.lan>
2021-09-08 23:49:45 +0200

    insertsprint

--- src/store/cred.js
+++ src/store/cred.js
@@ -2,7 +2,7 @@ export default {
     prod: {
         url_workitem_update: "https://tomasmali.it/boardapi/workitem/update",
         url_sprint_get: "https://tomasmali.it/boardapi/workitem/getSprints",
-        url_sprint_insert: "https://tomasmali.it/boardapi/insertSprint",
+        url_sprint_insert: "https://tomasmali.it/boardapi/workitem/insertSprint",
         url_workitem_get: "https://tomasmali.it/boardapi/workitem/get",
         url_wrkitem_insert: "https://tomasmali.it/boardapi/workitem/insertWorkitem",
         url_workitem_delete: "https://tomasmali.it/boardapi/workitem/deleteWorkitem",
